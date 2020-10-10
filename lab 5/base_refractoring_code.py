84ebb61d2f631337bc0c25634ed92182435a43ac
Alexey Akimov <akimov.a@phystech.edu>
2020-10-11 02:24:51 +0500

    added function documentation and changed leg drawing functions

--- lab 5/base_refractoring_code.py
+++ lab 5/base_refractoring_code.py
@@ -3,25 +3,41 @@ from pygame.draw import *
 import math
 
 
-def man(screen, x, y, width, height):
+def draw_man(screen, x, y, width, height):
+    '''
+    Рисует мужчину в опорной точке (x, y),
+    которая находится в верхнем левом углу прямоугольника,
+    описанного вокруг туловища формы эллипса.
+    Параметр width - ширина мужчины.
+    Параметр height - высота мужчины.
+    '''
     ellipse(screen, COLOR['PURPLE'], (x, y, width, height))
     face(screen, x, y, width, height)
-    hand_man(screen, x, y, width, height)
-    leg_man(screen, x, y, width, height)
+    hands_man(screen, x, y, width, height)
+    legs_man(screen, x, y, width, height)
 
 
-def woman(screen, x, y, width, height):
-    # hairs
+def draw_woman(screen, x, y, width, height):
+    '''
+    Рисует женщину в опорной точке (x, y),
+    которая находится в верхнем левом углу прямоугольника,
+    описанного вокруг туловища формы треугольника.
+    Параметр width - ширина женщины.
+    Параметр height - высота женщины.
+    '''
     circle(screen, COLOR['BROWN'], (x + width // 2, y - height // 5), height // 4)
     rect(screen, COLOR['BROWN'], (x + int(width * 0.08), y - height // 5, height // 2, height // 2))
     polygon(screen, COLOR['PINK'],
             [(x + width // 2, y), (x, y + height), (x + width, y + height)])
     face(screen, x, y, width, height)
-    hand_woman(screen, x, y, width, height)
-    leg_woman(screen, x, y, width, height)
+    hands_woman(screen, x, y, width, height)
+    legs_woman(screen, x, y, width, height)
 
     
 def face(screen, x, y, width, height):
+    '''
+    Рисует лицо относитльно параметров, заданных для мужчины или женщины.
+    '''
     circle(screen, COLOR['FACE_COLOR'], (x + width // 2, y - height // 6), height // 5)
     eyes(screen, x, y, width, height)
     nose(screen, x, y, width, height)
@@ -29,6 +45,9 @@ def face(screen, x, y, width, height):
     
 
 def eyes(screen, x, y, width, height):
+    '''
+    Рисует глаза относитльно параметров, заданных для мужчины или женщины.
+    '''
     circle(screen, COLOR['WHITE'], (x + int(0.33 * width), y - int(0.22 * height)), height // 17)
     circle(screen, COLOR['BLACK'], (x + int(0.33 * width), y - int(0.22 * height)), height // 34)    
     circle(screen, COLOR['WHITE'], (x + int(0.67 * width), y - int(0.22 * height)), height // 17)
@@ -36,21 +55,33 @@ def eyes(screen, x, y, width, height):
         
 
 def nose(screen, x, y, width, height):
+    '''
+    Рисует нос относитльно параметров, заданных для мужчины или женщины.
+    '''
     line(screen, COLOR['BLACK'], (x + width // 2, y - int(0.2 * height)), (x + width // 2, y - int(height * 0.2)), 1)
     
 
 def mouth(screen, x, y, width, height):
+    '''
+    Рисует рот относитльно параметров, заданных для мужчины или женщины.
+    '''
     arc(screen, COLOR['BLACK'], 
         (x + int(0.35 * width), y - int(0.18 * height), int(0.3 * width), height // 6), 
         1.1 * math.pi, 1.9 * math.pi, 2)
     
 
-def hand_man(screen, x, y, width, height):
+def hands_man(screen, x, y, width, height):
+    '''
+    Рисует руки относитльно параметров, заданных для мужчины.
+    '''
     line(screen, COLOR['BLACK'], (x + width // 5, y + height // 10), (x - width // 2, y + int(0.65 * height)), 2)
     line(screen, COLOR['BLACK'], (x + int(0.8 * width), y + height // 10), (x + int(1.6 * width), y + height // 2))
 
     
-def hand_woman(screen, x, y, width, height):
+def hands_woman(screen, x, y, width, height):
+    '''
+    Рисует руки относитльно параметров, заданных для женщины.
+    '''
     line(screen, COLOR['BLACK'], 
          (x + width//5, y + height//10), (x - width // 2, y + int(0.65 * height)), 2)
     line(screen, COLOR['BLACK'],
@@ -59,26 +90,40 @@ def hand_woman(screen, x, y, width, height):
          (x + int(1.3 * width), y + int(0.3 * height)), (x + int(1.9 * width), y + height // 4), 2)
 
     
-def leg_man(screen, x, y, width, height):
-    line(screen, COLOR['BLACK'],
+def legs_man(screen, x, y, width, height):
+    '''
+    Рисует ноги относитльно параметров, заданных для мужчины.
+    '''
+    legs_man_param = [
+                      ((x + width // 3, y + int(0.95 * height)), (x + width // 20, y + int(1.5 * height))),
+                      ((x + width // 20, y + int(1.5 * height)), (x - width // 4, y + int(1.5 * height) + 2)),
+                      ((x + 2 * width // 3, y + int(0.95 * height)), (x + 3 * width // 4, y + int(1.49 * height))),
+                      ((x + 3 * width//4, y + int(1.49 * height)), (x + width, y + int(1.5 * height)))
+                      ]
+    for i_m in range(4):
+        line(screen, COLOR['BLACK'], legs_man_param[i_m][0], legs_man_param[i_m][1], 2)
+    '''line(screen, COLOR['BLACK'],
          (x + width // 3, y + int(0.95 * height)), (x + width // 20, y + int(1.5 * height)), 2)
     line(screen, COLOR['BLACK'],
          (x + width // 20, y + int(1.5 * height)), (x - width // 4, y + int(1.5 * height) + 2), 2)
     line(screen, COLOR['BLACK'],
          (x + 2 * width // 3, y + int(0.95 * height)), (x + 3 * width // 4, y + int(1.49 * height)), 2)
     line(screen, COLOR['BLACK'],
-         (x + 3 * width//4, y + int(1.49 * height)), (x + width, y + int(1.5 * height)), 2)
+         (x + 3 * width//4, y + int(1.49 * height)), (x + width, y + int(1.5 * height)), 2)'''
     
     
-def leg_woman(screen, x, y, width, height):
-    line(screen, COLOR['BLACK'],
-            (x + width // 3, y + height), (x - width // 10, y + int(1.45 * height)), 2)
-    line(screen, COLOR['BLACK'],
-            (x - width // 10, y + int(1.45 * height)), (x - int(0.4 * width), y + int(1.45 * height)), 2)
-    line(screen, COLOR['BLACK'],
-            (x + int(width * 0.6), y + height), (x + int(width * 0.8), y + int(1.45 * height)), 2)
-    line(screen, COLOR['BLACK'],
-            (x + int(width * 0.8), y + int(1.45 * height)), (x + int(1.1 * width), y + int(1.5 * height)), 2)
+def legs_woman(screen, x, y, width, height):
+    '''
+    Рисует ноги относитльно параметров, заданных для женщины.
+    '''
+    legs_woman_param = [
+                        ((x + width // 3, y + height), (x - width // 10, y + int(1.45 * height))),
+                        ((x - width // 10, y + int(1.45 * height)), (x - int(0.4 * width), y + int(1.45 * height))),
+                        ((x + int(width * 0.6), y + height), (x + int(width * 0.8), y + int(1.45 * height))),
+                        ((x + int(width * 0.8), y + int(1.45 * height)), (x + int(1.1 * width), y + int(1.5 * height)))
+                        ]
+    for i_w in range(4):
+        line(screen, COLOR['BLACK'], legs_woman_param[i_w][0], legs_woman_param[i_w][1], 2)
     
     
 def balloons(screen, x, y, angle, size=80):
@@ -123,18 +168,21 @@ COLOR = {
 screen_width, screen_height = 1032, 768
 
 screen = pygame.display.set_mode((screen_width, screen_height))
-screen.fill(COLOR['GREEN'])
-# sky
+#draw grass
+rect(screen, COLOR['GREEN'], (0, 0, screen_width, screen_height))
+# draw sky
 rect(screen, COLOR['BLUE'], (0, 0, screen_width, screen_height // 2))
 
 # right couple
-man(screen, 130, 380, 100, 170)
-woman(screen, 320, 360, 100, 170)
+draw_man(screen, 130, 380, 100, 170)
+draw_woman(screen, 320, 360, 100, 170)
+
+#for drawing reflected objects
 new = pygame.transform.flip(screen, True, False)
 screen.blit(new, (0, 0))
 # left couple
-man(screen, 130, 380, 100, 170)
-woman(screen, 320, 360, 100, 170)
+draw_man(screen, 130, 380, 100, 170)
+draw_woman(screen, 320, 360, 100, 170)
 
 line(screen, COLOR['BLACK'], (80, 510), (65, 300), 2)
 balloons(screen, 65, 300, 50)
